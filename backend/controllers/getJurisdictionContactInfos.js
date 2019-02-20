bde48ccb26af83271fd23c3e364eb266d6feda7f
Rayed Benbrahim <rayed.benbrahim@gmail.com>
2019-02-20 10:40:50 +0100

    completed exercice 1

--- backend/controllers/getJurisdictionContactInfos.js
+++ backend/controllers/getJurisdictionContactInfos.js
@@ -1,15 +1,98 @@
 const { db } = require("./../models/dbInit");
 
-module.exports = function getJurisdictionContactInfos(req, res, next) {
-  let jurisdiction_id = req.query.jurisdiction_id;
-  let contactInfos = Object.create(null);
+module.exports = async function getJurisdictionContactInfos(req, res, next) {
+  try {
+    const rawData = await getJurisdictionInfo(req.params)
+    await res.json({ contactInfos: rawData })
+  } catch (err) {
+    res.status(500).json({error: "Something went wrong", message: err})
+  }
+  
+}
 
+const getJurisdictionInfo = async (httpParams) => {
+  let jurisdiction_id = httpParams.jurisdiction_id
+  await findJurisdictionInDB(jurisdiction_id)
+  return contact_infos
+}
 
-  //INSERT YOUR CODE HERE
-  return res.json({
-    contactInfos : {
-      telephone: []
+const findJurisdictionInDB = async (jurisdiction_id) => {
+  const SQLQuery = `
+  SELECT jurisdictions.jurisdiction_id, jurisdictions.telephone, jurisdictions.fax, jurisdictions.email,jurisdictions_verified_contact_infos.type, jurisdictions_verified_contact_infos.data
+  FROM jurisdictions
+  LEFT JOIN jurisdictions_verified_contact_infos
+  ON jurisdictions.jurisdiction_id = jurisdictions_verified_contact_infos.jurisdiction_id
+  WHERE jurisdictions.jurisdiction_id = ?
+  `
+  await db.all(SQLQuery,jurisdiction_id,(err,row) => {
+    if (err) { throw err }
+    serializeJurisdiction(row)
+  })
+}
+
+const serializeJurisdiction = (allJurisdictions) => {
+  const serializedJurisdiction = {}
+  serializedJurisdiction.telephone = []
+  serializedJurisdiction.email = []
+  serializedJurisdiction.fax = []
+
+  const verifiedContacts = {}
+  verifiedContacts.telephone = false
+  verifiedContacts.fax = false
+  verifiedContacts.email = false
+  for (let i = 0; i < allJurisdictions.length; i++){
+    const jurisdictionVerifiedData = processVerifiedData(allJurisdictions[i])
+    serializedJurisdiction[jurisdictionVerifiedData.type].push({data: jurisdictionVerifiedData.data, verified: jurisdictionVerifiedData.verified})
+    if (emailIsDuplicate(allJurisdictions[i])) {
+      verifiedContacts.email = true
     }
-  });
-};
+    if (faxIsDuplicate(allJurisdictions[i])){
+      verifiedContacts.fax = true
+    }
+    if (telephoneIsDuplicate(allJurisdictions[i])){
+      verifiedContacts.telephone = true
+    }      
+  }
+  if (verifiedContacts.telephone == false && allJurisdictions[0].telephone != null) {
+    serializedJurisdiction.telephone.push({data: allJurisdictions[0].telephone, verified: false})
+  }  
+  if (verifiedContacts.fax == false && allJurisdictions[0].fax != null) {
+    serializedJurisdiction.fax.push({data: allJurisdictions[0].fax, verified: false})
+  }  
+  if (verifiedContacts.email == false && allJurisdictions[0].email != null) {
+    serializedJurisdiction.email.push({data: allJurisdictions[0].email, verified: false})
+  }  
+  setValueInObject(serializedJurisdiction)
+}
 
+const processVerifiedData = (verifiedData) => {
+  const dataType = Object.values(verifiedData)[4]
+  const dataValue = Object.values(verifiedData)[5]
+  const verifiedContactDetails = {type: dataType, data: dataValue, verified: true}
+  return verifiedContactDetails
+}
+
+const emailIsDuplicate = (jurisdictionData) => {
+  if (jurisdictionData.type == 'email' && jurisdictionData.email == jurisdictionData.data) {
+    return true
+  }
+  return false
+}
+
+const telephoneIsDuplicate = (jurisdictionData) => {
+  if (jurisdictionData.type == 'telephone' && jurisdictionData.telephone == jurisdictionData.data) {
+    return true
+  }
+  return false
+}
+
+const faxIsDuplicate = (jurisdictionData) => {
+  if (jurisdictionData.type == 'fax' && jurisdictionData.fax == jurisdictionData.data) {
+    return true
+  }
+  return false
+}
+
+const setValueInObject = (value) => {
+  contact_infos = value
+}
